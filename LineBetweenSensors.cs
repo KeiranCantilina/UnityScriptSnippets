724a24054c733b816de3c92f2866597feffbf1f6
KeiranCantilina <keirancantilina@gmail.com>
2023-05-10 14:46:13 -0400

    Add files via upload
    Script to set Unity LineRenderer nodes based on the dynamic positions of a static array of gameobjects.

---
+++ LineBetweenSensors.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineBetweenSensors : MonoBehaviour
+{
+    public GameObject[] TrackingPoints; // The tracking sensor objects; remember to assign in inspector!
+    public LineRenderer lineRenderer;   // The line renderer component; remember to assign in inspector!
+    
+    // Start is called before the first frame update
+    void Start(){
+
+        // Set the position count of the linerenderer
+        lineRenderer.positionCount = TrackingPoints.Length;
+
+        // Get the transforms of the objects and set the positions of the LineRenderer using them
+        for (int i = 0;i<lineRenderer.positionCount;i++){
+            lineRenderer.SetPosition(i, TrackingPoints[i].transform.position);
+        }
+
+    }
+
+    
+    // Update is called once per frame
+    void Update(){
+       
+    }
+}
