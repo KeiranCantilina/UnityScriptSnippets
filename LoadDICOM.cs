62745cb3981c3edf84fe6bdf7e115830c2c36464
KeiranCantilina <keirancantilina@gmail.com>
2024-03-12 12:02:41 -0400

    Add files via upload

--- LoadDICOM.cs
+++ LoadDICOM.cs
@@ -5,6 +5,9 @@ using TMPro;
 using DICOMParser;
 using DICOMViews;
 using System.IO;
+using Threads;
+using System.Threading;
+using System;
 
 
 public class LoadDICOM : MonoBehaviour
@@ -16,49 +19,93 @@ public class LoadDICOM : MonoBehaviour
     private Texture2D texture;
     private int sliceindex;
     public SliceType sliceType;
+    private SliceType oldSliceType;
     public GameObject displayCanvas;
+    private ThreadGroupState ThreadState;
+    private string DebugMessage = "Ready to Load";
+    private int processingStage = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         sliceindex = 144;
         folderpath = Application.dataPath + "\\Datasets\\1156_DICOM";
+        oldSliceType = sliceType;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        // Refresh texture display if slice type is changed
+        if(oldSliceType != sliceType)
+        {
+            oldSliceType = sliceType;
+            processingStage = 6;
+        }
+
+        // Process Pseudo-Queue
+        if(processingStage == 2)
+        {
+            processingStage = 3;
+            StartPreProcess();
+        }
+        else if(processingStage == 4){
+            processingStage = 5;
+            StartCreatingTextures();
+        }
+        else if(processingStage == 6)
+        {
+            processingStage = 0;
+            StartGetTexture2D();
+        }
+        if(processingStage != 0 && processingStage != 6)
+        {
+            imagestack.WriteDebug(DebugMessage + "\n" + "Progress: " + 100 * ThreadState.Progress / ThreadState.TotalProgress + "%");
+        }
+        else
+        {
+            imagestack.WriteDebug(DebugMessage);
+        }
+        
     }
 
+    // Triggered by button in UI
     public void LoadFile()
     {
-        imagestack.StartParsingFiles(folderpath);
-        
-        //imagestack.StartPreprocessData();
-        //imagestack.StartCreatingTextures();
+        processingStage = 1;
+        DebugMessage = "Loading files...";
+        ThreadState = imagestack.StartParsingFiles(folderpath);
+        ThreadState.ThreadsFinished += IncrementProcessCounter;
     }
 
-    public void startPreProcess()
+    public void StartPreProcess()
     {
-        imagestack.StartPreprocessData();
+        DebugMessage = "Preprocessing...";
+        ThreadState = imagestack.StartPreprocessData();
+        ThreadState.ThreadsFinished += IncrementProcessCounter;
     }
 
-    public void startCreatingTextures()
+    public void StartCreatingTextures()
     {
-        imagestack.StartCreatingTextures();
+        DebugMessage = "Creating Textures...";
+        ThreadState = imagestack.StartCreatingTextures();
+        ThreadState.ThreadsFinished += IncrementProcessCounter;
     }
 
-    public void GetTexture2D()
+    // Triggered by changing slice type or index (or at end of load file process)
+    public void StartGetTexture2D()
     {
         texture =  imagestack.GetTexture2D(sliceType, sliceindex);
         displayCanvas.GetComponent<Renderer>().material.SetTexture("_MainTex", texture);
         texture.Apply();
+        DebugMessage = "Showing " + sliceType + " View, Slice No. " + sliceindex;
     }
 
     public void incrementSliceIndex()
     {
         sliceindex++;
+        processingStage = 6;
     }
 
     public void decrementSliceIndex()
@@ -66,10 +113,15 @@ public class LoadDICOM : MonoBehaviour
         if(sliceindex > 0)
         {
             sliceindex--;
+            processingStage = 6;
         }
         else
         {
             sliceindex = 0;
         }
+    }
+    private void IncrementProcessCounter(object sender, EventArgs e)
+    {
+        processingStage++;
     }
 }
