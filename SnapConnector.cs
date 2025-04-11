98a1afc7ba70511e84a2d79a5bb00982b8f9bb03
KeiranCantilina <keirancantilina@gmail.com>
2025-04-11 17:35:43 -0400

    Add files via upload

--- SnapConnector.cs
+++ SnapConnector.cs
@@ -14,9 +14,9 @@ public class SnapConnector : MonoBehaviour
     public bool isFemaleConnector;
     private bool snapped = false;
     private GameObject SnappingParent;
+    private GameObject CollidingWith;
     private GameObject myParent;
-    //public Collider[] SnapAnchorColliders;
-    public Vector3 SnapOffset;
+    //public Vector3 SnapOffset; // No longer needed because we automatically calculate snap offsets based on snap connector positions
     public Vector3 RotationOffset;
     private Quaternion savedOrientation;
     private bool isBeingDragged = false;
@@ -24,11 +24,15 @@ public class SnapConnector : MonoBehaviour
     private bool triggered = false;
     public float snapping_speed = 1.0f;
     private string allowableSnappingTag;
+    public Vector3 ManualPositionOffset;
 
     // Start is called before the first frame update
     void Start()
     {
+        // We're allowed to snap with anything else with this tag
         allowableSnappingTag = this.tag;
+
+        // Get default parent transform and associated stuff (should be object this snap connector is attached to)
         myParent = this.transform.parent.gameObject.transform.parent.gameObject;
         DraggingScript = this.transform.parent.gameObject.GetComponent<DevLocker.PhysicsUtils.DragRigidbodyBetter>();
     }
@@ -36,20 +40,32 @@ public class SnapConnector : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // update dragging status
         isBeingDragged = DraggingScript.getDraggingStatus();
+
+        // If we're in snapping range and we're not being dragged:
         if (triggered == true && !isBeingDragged)
         {
-            // Snap!
+            // Set snapping animation distance per frame
             var step = snapping_speed * Time.deltaTime;
-            myParent.transform.position = Vector3.MoveTowards(myParent.transform.position, SnappingParent.transform.position + SnappingParent.transform.TransformDirection(SnapOffset), step);
+
+            //Get snap offsets from snapping connector local positions, then transform to global coords
+            Vector3 SnapOffsetFromOther = SnappingParent.transform.InverseTransformPoint(CollidingWith.transform.position);
+            Vector3 SnapOffsetFromThis = myParent.transform.InverseTransformPoint(this.transform.position);
+
+            // Snap!
+            myParent.transform.position = Vector3.MoveTowards(myParent.transform.position, SnappingParent.transform.position + SnappingParent.transform.TransformDirection(-SnapOffsetFromThis + SnapOffsetFromOther + ManualPositionOffset), step); // Changed
             myParent.transform.rotation = Quaternion.RotateTowards(myParent.transform.rotation, SnappingParent.transform.rotation, step*100);
 
+            // If we're close enough, consider us snapped
             if(Vector3.Distance(myParent.transform.position, SnappingParent.transform.position) < 0.001f)
             {
                 snapped = true;
 
             }
         }
+
+        // If we're not snapped but we're in snapping range, Look At 
         if(triggered == true && snapped == false && isBeingDragged)
         {
             //Make objects face eachother
@@ -67,7 +83,9 @@ public class SnapConnector : MonoBehaviour
         {
             UnityEngine.Debug.Log("Triggered!\n");
             triggered = true;
+            CollidingWith = other.gameObject;
             SnappingParent = other.gameObject.transform.parent.gameObject.transform.parent.gameObject;
+
             // SnappingParent becomes actual parent here
             myParent.transform.parent = SnappingParent.transform;
         }
